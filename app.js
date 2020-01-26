52b7fe077f4761be366a32032e516c4ef2c97fcb
Ma7eer <maher.alkendi2@gmail.com>
2020-01-26 15:51:43 -0800

    preparing to host on heroku

--- app.js
+++ app.js
@@ -17,7 +17,22 @@ const employeesRouter = require("./routes/employees");
 
 const app = express();
 
-app.use(cors());
+const whitelist = [
+  "https://gracious-carson-3c0254.netlify.com",
+  "http://localhost:3000"
+];
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(helmet());
 app.use(express.json());
