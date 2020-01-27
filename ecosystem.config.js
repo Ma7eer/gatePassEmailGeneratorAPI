6fcccf314e443569afd9980519b675bb4129a9f0
Ma7eer <maher.alkendi2@gmail.com>
2020-01-26 18:11:09 -0800

    changed start directory

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "app",
-      script: "./app.js",
+      script: "./bin/www",
       instances: "max",
       env: {
         NODE_ENV: "development"
