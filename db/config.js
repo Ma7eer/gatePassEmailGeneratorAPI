1824b4cff964f909d42049d741476a18fc86e0b6
Ma7eer <maher.alkendi2@gmail.com>
2020-01-26 18:48:01 -0800

    addding item

--- db/config.js
+++ db/config.js
@@ -6,7 +6,8 @@ const client = new pg.Client(conString);
 
 client.connect(err => {
   if (err) {
-    res.status(500).json({ message: "Could not connect to db" });
+    // res.status(500).json({ message: "Could not connect to db" });
+    console.log(err);
   }
   console.log("connected to db");
 });
