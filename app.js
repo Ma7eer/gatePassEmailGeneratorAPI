cbb4f111568a3b8a99b52f022992eb702e64270f
Ma7eer <maher.alkendi2@gmail.com>
2020-02-14 05:48:29 -0800

    prod server set

--- app.js
+++ app.js
@@ -17,23 +17,23 @@ const employeesRouter = require("./routes/employees");
 
 const app = express();
 
-// const whitelist = [
-//   "https://gracious-carson-3c0254.netlify.com",
-//   "https://gracious-carson-3c0254.netlify.com/Home",
-//   "http://localhost:3000"
-// ];
+const whitelist = [
+  "https://gracious-carson-3c0254.netlify.com",
+  "https://gracious-carson-3c0254.netlify.com/Home",
+  "http://localhost:3000"
+];
 
-// const corsOptions = {
-//   origin: function(origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   }
-// };
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(helmet());
 app.use(express.json());
